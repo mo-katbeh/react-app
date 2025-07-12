206666a0c2c4f1704050850706f6d7d8731913c1
mo-katbeh <hamode.katbeh@gmail.com>
2025-07-12 14:30:19 +0300

    end of form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,40 +1,83 @@
 import React, { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be more than 3 characters" }),
+  age: z.number().min(12),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
   // const nameRef = useRef<HTMLInputElement>(null);
   // const ageRef = useRef<HTMLInputElement>(null);
   // const person = { name: "", age: 0 };
   // //useSate
-  const [person, setPerson] = useState({
-    name: "",
-    age: 0,
-  });
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if (nameRef.current !== null) {
-      person.name = nameRef.current.value;
-    }
-    if (ageRef.current !== null) {
-      person.age = parseInt(ageRef.current.value);
-    }
-    console.log(person);
-  };
+  // const [person, setPerson] = useState({
+  //   name: "",
+  //   age: "",
+  // });
+
+  // const handleSubmit = (event: FormEvent) => {
+  //   event.preventDefault();
+  //   // if (nameRef.current !== null) {
+  //   //   person.name = nameRef.current.value;
+  //   // }
+  //   // if (ageRef.current !== null) {
+  //   //   person.age = parseInt(ageRef.current.value);
+  //   // }
+  //   console.log(person);
+  // };
+  //useForm
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  // console.log(register("name"));
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        {/* <input ref={nameRef} id="name" type="text" className="form-control" /> */}
+        <input
+          // value={person.name}
+          // onChange={(event) =>
+          //   setPerson({ ...person, name: event.target.value })
+          // }
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name && <p className="text-danger"> {errors.name.message} </p>}
       </div>
       {/* dev.mb-3>label.form-label+input[type=number].form-control */}
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        {/* <input ref={ageRef} id="age" type="number" className="form-control" /> */}
+        <input
+          // value={person.age}
+          // onChange={(event) =>
+          //   setPerson({ ...person, age: parseInt(event.target.value) })
+          // }
+          {...register("age", { valueAsNumber: true })}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+        {errors.age && <p className="text-danger"> {errors.age.message} </p>}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
